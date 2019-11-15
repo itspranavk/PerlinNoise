a90c82d5174d6c30d77cbb5af872234d03be662d
Pranav Kulkarni <pranavkop@live.com>
2019-11-15 03:07:32 -0500

    fixed bug

--- visualizer.py
+++ visualizer.py
@@ -20,28 +20,32 @@ def f(x, y, p):
         z_final.append(z)
     return np.asarray(z_final)
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
+def render():
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
 
-points = generate_points()
+    points = generate_points()
 
-X = []
-Y = []
-for i in range(len(points)):
-    X.append([])
-    Y.append([])
-    for j in range(len(points)):
-        X[i].append(i)
-        Y[i].append(j)
+    X = []
+    Y = []
+    for i in range(len(points)):
+        X.append([])
+        Y.append([])
+        for j in range(len(points)):
+            X[i].append(i)
+            Y[i].append(j)
 
-x = np.reshape(X, (len(points), len(points)))
-y = np.reshape(Y, (len(points), len(points)))
-z = np.reshape(points, (len(points), len(points)))
+    x = np.reshape(X, (len(points), len(points)))
+    y = np.reshape(Y, (len(points), len(points)))
+    z = np.reshape(points, (len(points), len(points)))
 
-ax.plot_surface(x, y, z)
+    ax.plot_surface(x, y, z)
 
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
 
-plt.show()
+    plt.show()
+
+
+render()
